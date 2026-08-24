f123a1ef41e05820058c8fd6e1aff5fb8882fd0e
Subham Sahu <subhamsahu7248@gmail.com>
2026-08-24 17:21:32 +0530

    Fix frontend CORS for Vite ports

--- backend/src/main/java/rajdhani_care_backend/BookingController.java
+++ backend/src/main/java/rajdhani_care_backend/BookingController.java
@@ -19,7 +19,9 @@ import java.util.List;
         origins = {
                 "http://localhost:5173",
                 "http://localhost:5174",
-                "http://localhost:5175"
+                "http://localhost:5175",
+                "http://localhost:5176",
+                "http://localhost:5177"
         },
         methods = {
                 RequestMethod.GET,
